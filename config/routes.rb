4f7caa45af1ddf2a5b38c31d92899b91a82f791e
Adam Thayer <adamjthayer@gmail.com>
2016-06-13 14:16:02 -0700

    Setting up Api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
+require 'api_constraints.rb'
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1,
+            constraints: ApiConstraints.new(version: 1, default: true) do
     # We are going to list our resources here
+    end
   end
 end
